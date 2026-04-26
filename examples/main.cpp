397ae3d3c75d46493d4d619584fd6edd1eca7521
StanislavShypilov <shypilovstanislav@gmail.com>
2026-04-26 20:09:33 +0300

    Main Logic

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <cstdlib>
+#include <string.h>
 #include "Calculator.h"
 
 using namespace std;
 
-int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+int main(int argc, char** argv) {
+    if (argc != 4 || strlen(argv[2]) != 1) {
+        cout << "Can't u check ur parameters before writing them >:(((" << endl;
+        return 1;
+    }
+    double a = atof(argv[1]), b = atof(argv[3]), res;
+    Calculator calc;
+    switch (*argv[2]) {
+    case '+': res = calc.add(a, b);
+        break;
+    case '-': res = calc.sub(a, b);
+        break;
+    case '*': res = calc.mul(a, b);
+        break;
+    case '/': res = calc.div(a, b);
+        break;
+    default: {
+        cout << "This operation does not exist :((((";
+        return 1;
+    }
+    }
+    cout << res << endl;
+    return 0;
 }
