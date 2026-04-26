8a5187fca2670b139f84a7bd5e08880ddc855d7e
holysqualor <artmkokoko@gmail.com>
2026-04-26 17:37:20 +0300

    Add Calculator.h

---
+++ include/Calculator.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+    double add(double a, double b);
+    double sub(double a, double b);
+    double mul(double a, double b);
+    double div(double a, double b);
+};
+
+#endif
